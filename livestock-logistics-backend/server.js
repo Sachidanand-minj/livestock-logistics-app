d9c0adb8f51450e8265b7092e7d011f2dee710d6
Sachidanand-minj <149020683+Sachidanand-minj@users.noreply.github.com>
2025-04-25 15:57:10 +0530

    Update server.js

--- livestock-logistics-backend/server.js
+++ livestock-logistics-backend/server.js
@@ -28,6 +28,9 @@ app.use('/uploads', express.static('uploads'));
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
+app.get('/', (req, res) => {
+  res.send('🚀 Livestock Logistics Backend is running!');
+});
 
 // Socket Setup
 require('./sockets/socket')(server);
